a94290514271b37c44cbc6bb815bfa8f2405ba15
Kamran Ali <kgali21@gmail.com>
2019-07-18 11:06:22 -0700

    ginished with non-input betting

--- src/rps-game.js
+++ src/rps-game.js
@@ -7,49 +7,105 @@ const message = document.getElementById('message');
 const winCount = document.getElementById('win-count');
 const lossCount = document.getElementById('loss-count');
 const drawCount = document.getElementById('draw-count');
+const userBetCount = document.getElementById('user-bet');
+const cpuBetCount = document.getElementById('cpu-bet');
 
 let wins = 0;
 let losses = 0;
 let draws = 0;
+let userBetAmount = 100;
+let cpuBetAmount = 100;
+
+let userMsg = userBetAmount;
+let cpuMsg = cpuBetAmount;
+
+userBetCount.textContent = userMsg;
+cpuBetCount.textCOntent = cpuMsg;
 
 throwButton.addEventListener('click', () => {
     const cpuChoice = getThrow();
    
+    
     winImage.classList.remove('invisible');
     const src = 'assets/weapons/' + cpuChoice + '.jpg';
     winImage.src = src;
-
+    
     const weaponButton = document.querySelector('input:checked');
     const userChoice = weaponButton.value;
     
+    let loseResponse = 'Game Over...';
+    let winResponse = 'You won the Millenium Falcon';
 
     if(userChoice === cpuChoice){
         message.textContent = 'draw';
         draws++;
         drawCount.textContent = draws;
+        userBetAmount += 0;
+        userBetCount.textContent = userBetAmount + ' Credits';
+        cpuBetAmount += 0;
+        cpuBetCount.textContent = cpuBetAmount + ' Credits';
     } else if(weaponButton.value === 'rock' && cpuChoice === 'scissors') {
-        message.textContent = 'you win!';
+        message.textContent = 'YOU Win!';
         wins++;
         winCount.textContent = wins;
+        userBetAmount += 10;
+        userBetCount.textContent = userBetAmount + ' Credits';
+        cpuBetAmount += -10;
+        cpuBetCount.textContent = cpuBetAmount + ' Credits';
     } else if(userChoice === 'rock' && cpuChoice === 'paper') {
-        message.textContent = 'you lose!';
+        message.textContent = 'CPU Wins!';
         losses++;
         lossCount.textContent = losses;
+        userBetAmount += -10;
+        userBetCount.textContent = userBetAmount + ' Credits';
+        cpuBetAmount += 10;
+        cpuBetCount.textContent = cpuBetAmount + ' Credits';
     } else if(userChoice === 'paper' && cpuChoice === 'rock') {
-        message.textContent = 'you win!';
+        message.textContent = 'YOU Win!';
         wins++;
         winCount.textContent = wins;
+        userBetAmount += 10;
+        userBetCount.textContent = userBetAmount + ' Credits';
+        cpuBetAmount += -10;
+        cpuBetCount.textContent = cpuBetAmount + ' Credits';
     } else if(userChoice === 'paper' && cpuChoice === 'scissors'){
-        message.textContent = 'you lose...';
+        message.textContent = 'CPU Wins!';
         losses++;
         lossCount.textContent = losses;
+        userBetAmount += -10;
+        userBetCount.textContent = userBetAmount + ' Credits';
+        cpuBetAmount += 10;
+        cpuBetCount.textContent = cpuBetAmount + ' Credits';
     } else if(userChoice === 'scissors' && cpuChoice === 'paper') {
-        message.textContent = 'you win!';
+        message.textContent = 'YOU Win!';
         wins++;
         winCount.textContent = wins;
+        userBetAmount += 10;
+        userBetCount.textContent = userBetAmount + ' Credits';
+        cpuBetAmount += -10;
+        cpuBetCount.textContent = cpuBetAmount + ' Credits';
     } else if(userChoice === 'scissors' && cpuChoice === 'rock'){
-        message.textContent = 'you lose...';
+        message.textContent = 'CPU Wins!';
         losses++;
         lossCount.textContent = losses;
+        userBetAmount += -10;
+        userBetCount.textContent = userBetAmount + ' Credits';
+        cpuBetAmount += 10;
+        cpuBetCount.textContent = cpuBetAmount + ' Credits';
+    }
+
+    if(userBetAmount === 0){
+        userBetCount.textContent = loseResponse;
+    } else if(userBetAmount === 200) {
+        userBetCount.textContent = winResponse;
+        throwButton.disabled = true;
+
+    }
+    
+    if(cpuBetAmount === 0){
+        cpuBetCount.textContent = loseResponse;
+    } else if(cpuBetAmount === 200){
+        cpuBetCount.textContent = winResponse;
+        throwButton.disabled = true;
     }
 });
